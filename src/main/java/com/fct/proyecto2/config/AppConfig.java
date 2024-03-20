d8a5869305a2132fbd66a343f56a0b5e76b54be1
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-03-20 16:15:24 +0000

    feat:App config done

--- src/main/java/com/fct/proyecto2/config/AppConfig.java
+++ src/main/java/com/fct/proyecto2/config/AppConfig.java
@@ -1,5 +1,10 @@
 package com.fct.proyecto2.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.fct.proyecto2")
 public class AppConfig {
- 
+	
 }
--- src/main/java/com/fct/proyecto2/service/CocheServiceImpl.java
+++ src/main/java/com/fct/proyecto2/service/CocheServiceImpl.java
@@ -3,5 +3,5 @@ package com.fct.proyecto2.service;
 import com.fct.proyecto2.model.Coche;
 
 public interface CocheServiceImpl {
-	Coche crearCoche(String marca,String Modelo);
+	Coche crearCoche(String marca,String modelo);
 }
