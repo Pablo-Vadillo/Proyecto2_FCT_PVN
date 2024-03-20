123802c0599fc189030a73a84f55cdbbd68d5bc5
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-03-20 15:16:28 +0000

    feat:models done

--- src/main/java/com/fct/proyecto2/model/Coche.java
+++ src/main/java/com/fct/proyecto2/model/Coche.java
@@ -1,5 +1,38 @@
 package com.fct.proyecto2.model;
 
 public class Coche {
+	
+	private String marca;
+	private String modelo;
+	
+	public Coche(String marca, String modelo) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	
+	public Coche(){	
+	}
 
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	@Override
+	public String toString() {
+		return "Coche [marca=" + marca + ", modelo=" + modelo + "]";
+	}
+	
 }
